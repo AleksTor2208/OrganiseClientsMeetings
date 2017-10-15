3b7d62382ec8a8431e52ba0158968e0bba1cdf88
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-10-15 16:50:27 +0300

    Modify Meeting class.

--- OrganiseClientsMeetings/Models/Meeting.cs
+++ OrganiseClientsMeetings/Models/Meeting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,14 +9,17 @@ namespace OrganiseClientsMeetings.Models
     public class Meeting
     {
         public int Id { get; set; }
-        public int ClientId { get; set; }
-        public DateTime DateTime { get; set; }
+        public int ClientId { get; set; }              
+        public string DateTime { get; set; }
         public string Payment { get; set; }
+        public string Address { get; set; }
 
         /*
           Change 'isRemote property into Addres class in next iteration
          */
         public string isRemote { get; set; }
         public string Comment { get; set; }
+
+        
     }
 }
