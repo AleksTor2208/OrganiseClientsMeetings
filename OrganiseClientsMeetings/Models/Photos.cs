7b45080238df3e7a45bde33e2b79dfebc04e2cbd
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-10-28 22:35:43 +0300

    Add Photos class into Models.

---
+++ OrganiseClientsMeetings/Models/Photos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OrganiseClientsMeetings.Models
+{
+    public class Photos
+    {
+
+        [Key]
+        public int Id { get; set; }
+        public int MeetingsId { get; set; }
+        public string Photo1 { get; set; }
+        public string Photo2 { get; set; }
+        public string Photo3 { get; set; }
+        public string Photo4 { get; set; }
+        public string Photo5 { get; set; }
+    }
+}
