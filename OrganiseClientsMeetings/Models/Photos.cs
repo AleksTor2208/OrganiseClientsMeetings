b1d28f016dc9fdbec6364907ac6ed0a7473e95ab
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-10-29 13:21:22 +0300

    In Photos class add constructors and 'ValidateAndAssign' method.

--- OrganiseClientsMeetings/Models/Photos.cs
+++ OrganiseClientsMeetings/Models/Photos.cs
@@ -8,6 +8,17 @@ namespace OrganiseClientsMeetings.Models
 {
     public class Photos
     {
+        public Photos()
+        { }
+
+        public Photos(string photo1, string photo2, string photo3, string photo4, string photo5)
+        {
+            Photo1 = photo1;
+            Photo2 = photo2;
+            Photo3 = photo3;
+            Photo4 = photo4;
+            Photo5 = photo5;
+        }
 
         [Key]
         public int Id { get; set; }
@@ -16,6 +27,16 @@ namespace OrganiseClientsMeetings.Models
         public string Photo2 { get; set; }
         public string Photo3 { get; set; }
         public string Photo4 { get; set; }
-        public string Photo5 { get; set; }
+        public string Photo5 { get; set; }       
+
+        internal static Photos ValidateAndAssing(List<string> imageList)
+        {
+            var photo1 = imageList.Count >= 1 ? imageList[0] : null;
+            var photo2 = imageList.Count >= 2 ? imageList[1] : null;
+            var photo3 = imageList.Count >= 3 ? imageList[2] : null;
+            var photo4 = imageList.Count >= 4 ? imageList[3] : null;
+            var photo5 = imageList.Count == 5 ? imageList[4] : null;
+            return new Photos(photo1, photo2, photo3, photo4, photo5);
+        }
     }
 }
