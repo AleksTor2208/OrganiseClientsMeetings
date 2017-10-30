fb36821a2272c525017690cb1b2da91a4350df06
AleksTor2208 <aleksander.toropowski@gmail.com>
2017-10-30 21:12:42 +0300

    Add DataAnnotations in Client.cs

--- OrganiseClientsMeetings/Models/Client.cs
+++ OrganiseClientsMeetings/Models/Client.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,9 @@ namespace OrganiseClientsMeetings.Models
     public class Client
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
         public string Name { get; set; }
     }
 }
