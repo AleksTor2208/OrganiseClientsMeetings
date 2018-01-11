d8089ad95b327837dd112fcd3118fedd98995b4f
AleksTor2208 <aleksander.toropowski@gmail.com>
2018-01-11 08:41:19 +0100

    In HomeController Add functionality to check time availability, edit/delete methods

--- OrganiseClientsMeetings/Controllers/HomeController.cs
+++ OrganiseClientsMeetings/Controllers/HomeController.cs
@@ -21,6 +21,7 @@ namespace OrganiseClientsMeetings.Controllers
         {            
             _context = new ApplicationDbContext();
         }
+
         public ActionResult Index()
         {
             var meetingData = _context.Meetings.Select(m => m).ToArray();
@@ -37,7 +38,8 @@ namespace OrganiseClientsMeetings.Controllers
                     Id = item.Id,
                     Name = _context.Clients.Where(c => c.Id == item.ClientId).First().Name,
                     Date = item.Date,
-                    Time = item.Time,
+                    StartTime = item.StartTime,
+                    EndTime = item.EndTime,
                     Payment = item.Payment,
                     Address = item.Address,
                     Comment = item.Comment,
@@ -58,24 +60,44 @@ namespace OrganiseClientsMeetings.Controllers
         [HttpPost]
         public ActionResult AddMeeting(MeetingViewModel viewModel, IEnumerable<HttpPostedFileBase> files)
         {
-            if (viewModel == null) return Redirect("AddMeeting");
+            if (!RequiredDateNotNull(viewModel) && !IsTimeCorrectOrder(viewModel))
+                return Redirect("AddMeeting");                  
             var clientId = AddClient(viewModel.Name);
-            var photosId = AddPhotos(files);         
-            //rescale this image
+            var photosId = AddPhotos(files);
+
+            //future functionality: rescale this image
             //var resizedImage = resizeImage(Image.FromStream(HttpPostedFileBase.InputStream, true, true), new Size(50, 50));
 
             var meeting = new Meeting
             {
                 Date = viewModel.Date.ToString(),
-                Time = viewModel.Time.ToString(),
+                StartTime = viewModel.StartTime.ToString(),
+                EndTime = viewModel.EndTime.ToString(),
                 Payment = viewModel.Payment,
                 ClientId = clientId,
+                Comment = viewModel?.Comment,
                 Address = viewModel.Address,
                 PhotosId = photosId
             };         
             _context.Meetings.Add(meeting);           
             _context.SaveChanges();
             return Redirect("Index");
+        }
+
+        private bool IsTimeCorrectOrder(MeetingViewModel viewModel)
+        {
+            DateTime startTime;
+            DateTime endTime;
+            if (!DateTime.TryParse(viewModel.StartTime, out startTime)
+                || !DateTime.TryParse(viewModel.EndTime, out endTime)) return false;
+            var isProperOrder = DateTime.Compare(startTime, endTime);
+            return isProperOrder < 0;
+        }
+
+        private bool RequiredDateNotNull(MeetingViewModel viewModel)
+        {
+            return viewModel.Name != null && viewModel.Date != null && viewModel.StartTime != null
+                && viewModel.EndTime != null && viewModel.Payment != null && viewModel.Address != null;
         }
 
         private int AddPhotos(IEnumerable<HttpPostedFileBase> files)
@@ -111,15 +133,83 @@ namespace OrganiseClientsMeetings.Controllers
             return client.Id;
         }
 
-        public void CheckTimeAvailability(string Time, string Date)
+        public void CheckTimeAvailability(string StartTime, string EndTime, string Date)
         {
             var meetingData = _context.Meetings.Select(m => m).ToArray();
-            var res = "res";
-            foreach (var meeting in meetingData)
+            var DateTimeList = new List<DateRange>();
+            foreach (var stringDate in meetingData)
             {
+                var stringStartTime = $"{stringDate.Date} {stringDate.StartTime}";
+                var stringEndTime = $"{stringDate.Date} {stringDate.EndTime}";
 
+                DateTime endTime;
+                DateTime startTime;
+                if (DateTime.TryParse(stringStartTime, out startTime)
+                && DateTime.TryParse(stringEndTime, out endTime))         
+                    DateTimeList.Add( new DateRange(startTime, endTime));                
+            }     
+            
+            bool isAvailable = true;
+            foreach (var dateTime in DateTimeList)
+            {                
+                DateTime checkingValue;
+                DateTime.TryParse($"{Date} {StartTime}", out checkingValue);
+                if (checkingValue.IsNotInRange(dateTime.StartTime, dateTime.EndTime))
+                {
+                    isAvailable = false;
+                    break;
+                }                                                 
             }
-            Response.Write(res);
+            Response.Write(isAvailable.ToString());
+        }
+
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var meeting = _context.Meetings.First(m => m.Id == id);
+            var client = _context.Clients.First(c => c.Id == meeting.ClientId);
+            var photos = _context.Photos.First(p => p.Id == meeting.PhotosId);
+
+            var meetingViewModel = new MeetingViewModel
+            {
+                Id = id,
+                Name = client.Name,
+                Date = meeting.Date,
+                StartTime = meeting.StartTime,
+                EndTime = meeting.EndTime,
+                Address = meeting.Address,
+                Payment = meeting.Payment,
+                Comment = meeting.Comment,
+            };
+            return View(meetingViewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(MeetingViewModel viewModel)
+        {
+            var meeting = _context.Meetings.Find(viewModel.Id);
+            var client = _context.Clients.Find(meeting.ClientId);
+            _context.Meetings.Attach(meeting);
+            _context.Clients.Attach(client);    
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var meeting = _context.Meetings.First(m => m.Id == id);
+            var client = _context.Clients.First(c => c.Id == meeting.ClientId);
+            var clientPhotos = _context.Photos.First(p => p.Id == meeting.PhotosId);
+
+            _context.Meetings.Attach(meeting);
+            _context.Meetings.Remove(meeting);
+            _context.Clients.Attach(client);
+            _context.Clients.Remove(client);
+            _context.Photos.Attach(clientPhotos);
+            _context.Photos.Remove(clientPhotos);
+
+            _context.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
