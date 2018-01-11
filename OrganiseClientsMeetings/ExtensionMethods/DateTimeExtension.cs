c5dec9ce1d951796b6f4c9ad8899bc088c6ce227
AleksTor2208 <aleksander.toropowski@gmail.com>
2018-01-11 08:29:00 +0100

    Add extension method to check time availability

---
+++ OrganiseClientsMeetings/ExtensionMethods/DateTimeExtension.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrganiseClientsMeetings
+{
+    public static class DateTimeExtension
+    {
+        const int TimeSlippage = 40;
+
+        public static bool IsNotInRange(this DateTime dateToCheck, DateTime startDate, DateTime endDate)
+        {            
+            var isInRange = dateToCheck >= startDate 
+                && endDate.AddMinutes(TimeSlippage) <= dateToCheck;
+            return !isInRange;
+        }
+    }
+}
