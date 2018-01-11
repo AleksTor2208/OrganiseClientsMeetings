2e83402547ea7d2b54f2ab98d65cf6de7ab663d5
AleksTor2208 <aleksander.toropowski@gmail.com>
2018-01-11 08:35:55 +0100

    Add DateRange class

---
+++ OrganiseClientsMeetings/Models/DateRange.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OrganiseClientsMeetings.Models
+{
+    public class DateRange
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+
+        public DateRange(DateTime startTime, DateTime endTime)
+        {
+            this.StartTime = startTime;
+            this.EndTime = endTime;
+        }
+    }
+}
